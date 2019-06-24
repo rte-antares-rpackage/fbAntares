1e95327a2a20fab0f11817f6fd29d5f573405879
JulienBretteville <julien.bretteville@datastorm.fr>
2019-06-24 16:24:29 +0200

    Ouverture package

---
+++ R/getDist.R
@@ -0,0 +1,156 @@
+#' @title Adjust a polyhedron from a set of facets to approach a reference polyhedron
+#' 
+#' @description This function takes in input two polyhedrons, the reference one and
+#' the moving one (or set of facets). It returns in output a really close polyhedron,
+#' to the reference one, with the set of facets. It runs a minimization program 
+#' on a distance using the intersections of a set of lines and the two polyhedrons 
+#' and validate the moving polyhedron with a volume indicator.
+#' 
+#' @param A \code{data.table}, fix polyhedron, data.table containing at least 
+#' ram, Date, Period and two ptdf columns :
+#' \itemize{
+#'  \item ptdfAT : autrichian vertices
+#'  \item ptdfBE : belgium vertices
+#'  \item ptdfDE : german vertices
+#'  \item ptdfFR : french vertices
+#'  \item ram : line limits
+#'  \item Date : date in format YYYY-MM-DD
+#'  \item Period : hour in the day, between 1 and 24
+#' }
+#' @param A \code{data.table}, moving polyhedron, data.table containing at least 
+#' ram, Date, Period and two ptdf columns :
+#' \itemize{
+#'  \item ptdfAT : autrichian vertices
+#'  \item ptdfBE : belgium vertices
+#'  \item ptdfDE : german vertices
+#'  \item ptdfFR : french vertices
+#'  \item ram : line limits
+#'  \item Date : date in format YYYY-MM-DD
+#'  \item Period : hour in the day, between 1 and 24
+#' }
+#' @param nbLines \code{numeric}, number of half-lines used to compute the distance
+#' between the two polyhedra.
+#' @param maxiter \code{numeric}, maximum number of iteration, it is one of the 
+#' two stopping criteria of this function.
+#' @param thresholdIndic \code{numeric}, maximum number of iteration, it is one of the 
+#' two stopping criteria of this function.
+#' @examples
+#' 
+#' \dontrun{
+#' library(data.table)
+#' library(quadprog)
+#' library(linprog)
+#' polyhedra <- readRDS("~/RTE/fbAntares/inst/testdata/polyhedra.rds")
+#' A <- polyhedra[Date == "2019-02-14"]
+#' B <- polyhedra[Date == "2019-02-15"]
+#' nbLines <- 10000
+#' maxiter <- 20
+#' thresholdIndic <- 0.9
+#' quad <- F
+#' 
+#'  getBestPolyhedron(
+#'  A = A, B = B, nbLines = nbLines, maxiter = maxiter,
+#'  thresholdIndic = thresholdIndic, quad = quad)
+#' 
+#' }
+#' 
+#' @import data.table 
+#' @import quadprog
+#' @import linprog
+#' @importFrom stats rnorm runif
+#' 
+#' @export
+
+getBestPolyhedron <- function(A, B, nbLines, maxiter, thresholdIndic, quad = F) {
+  
+  col_ptdf <- colnames(A)[grep("ptdf", colnames(A))]
+  # .orderPtdfNames(A, B)
+  A <- copy(A)
+  dtLines <- .getNormalizedLines(nbLines = nbLines, dim = length(col_ptdf))
+  fixPlan <- .getIntersecPoints(dtLines, A)
+  PLANOUT <- copy(B)
+  
+  fixPlan <- fixPlan[order(Line_Coo_X1, Line_Coo_X2)]
+  movingPlan <- .getIntersecPoints(dtLines, PLANOUT)
+  movingPlan <- movingPlan[order(Line_Coo_X1, Line_Coo_X2)]
+  
+  for(k in 1:maxiter) {
+    newram <- .getDmatdvec(fixPlan, movingPlan, PLANOUT, col_ptdf, quad)
+    for (i in 1:nrow(PLANOUT)) {
+      if(newram[i] != 0) {
+        PLANOUT$ram[i] <- newram[i]
+      }
+    }
+    indic <- evalInter(PLANOUT, A)
+    
+    print(paste("Iteration", k, "indic :", indic))
+    movingPlan <- .getIntersecPoints(dtLines, PLANOUT)
+    movingPlan <- movingPlan[order(Line_Coo_X1, Line_Coo_X2)]
+    if (indic > thresholdIndic) {
+      return(PLANOUT)
+    }
+    
+  }
+  return(PLANOUT)
+}
+
+#######
+
+.getDmatdvec <- function(fixPlan, movingPlan, PLANOUT, col_ptdf, quad) {
+  dvec <- rep(0, length(PLANOUT$Face))
+  Dmat <- rep(0, length(PLANOUT$Face))
+  for(DD  in unique(PLANOUT$Face)){
+    Fb <- PLANOUT[Face == DD]
+    RO <- which(movingPlan$Face==DD)
+    movingPlan2 <- movingPlan[RO]
+    fixPlan2 <- fixPlan[RO]
+    colnames(fixPlan2)[grep("^X[1-9]", colnames(fixPlan2))]
+    ak <- as.matrix(fixPlan2[, .SD, .SDcols = colnames(fixPlan2)[
+      grep("^X[1-9]", colnames(fixPlan2))]])
+    Nai <- as.matrix(fixPlan2[, .SD, .SDcols = col_ptdf])
+    # bk <- as.matrix(movingPlan2[, .SD, .SDcols = colnames(fixPlan2)[
+    #   grep("^X[1-9]", colnames(movingPlan2))]])
+    
+    
+    if(length(RO)>1){
+      colnames(movingPlan2)[
+        grep("^Line_Coo_X[1-9]", colnames(fixPlan2))]
+      uk <- as.matrix(movingPlan2[, .SD, .SDcols = colnames(movingPlan2)[
+        grep("^Line_Coo_X[1-9]", colnames(fixPlan2))]])
+      Nbh <- as.vector(unlist(Fb[, .SD, .SDcols = col_ptdf]))
+      Nbh <- rep(Nbh,nrow(Nai))
+      Nbh <- matrix(Nbh, nrow = nrow(Nai), byrow = T)
+      
+      ukN <- rowSums(uk*Nbh)
+      # ukN <- matrix(rep(ukN, ncol(uk)), ncol = ncol(uk))
+      ukN <- uk / ukN
+      
+      dvec[DD] <- sum(fixPlan2$ram + rowSums(ak * (uk-Nai)))
+      Dmat[DD] <- sum(1 + rowSums(ukN * (uk - Nbh)))
+      
+    }
+  }
+  .launchOptim(Dmat, dvec, quad)
+}
+
+
+.launchOptim <- function(Dmat, dvec, quad) {
+  
+  if (quad) {
+    Dmat[Dmat == 0] <- 1
+    Dmat <- diag(Dmat, nrow = length(Dmat))
+    Amat <- diag(1, nrow = nrow(Dmat))
+    bvec <- rep(0, nrow(Dmat))
+    res <- solve.QP(Dmat = Dmat,
+                    dvec = dvec,
+                    Amat = Amat,
+                    bvec = bvec)
+  } else {
+    B <- diag(Dmat, nrow =length(Dmat))
+    res <- lp(direction = "min", objective.in = Dmat, const.mat = B, 
+              const.rhs = dvec, const.dir = ">=")
+  }
+  
+  hbhEvol <- round(res$solution)
+  hbhEvol
+}
---
+++ R/getIntersectionLinesHyperplanes.R
@@ -0,0 +1,93 @@
+.getNormalizedLines <- function(nbLines, dim) {
+  
+  # dtLines <- data.table(Line = 1:nbLines)
+  dtLines <- data.table(Line_Coo_X1 = rnorm(n = nbLines))
+  for (i in 1:dim) {
+    dtLines[, paste0("Line_Coo_X", i) := rnorm(n = nbLines)]
+  }
+  dtLines[, normvec := sqrt(rowSums(dtLines[, 1:dim]^2))]
+  for (i in 1:dim) {
+    dtLines[, paste0("Line_Coo_X", i) := get(paste0("Line_Coo_X", i))/normvec]
+  }
+  # dtLines[, normvec := sqrt(rowSums(dtLines[, 1:dim]^2))]
+  dtLines[, normvec := NULL]
+  return(dtLines)
+}
+
+
+
+
+.getIntersecPoints <- function(dtLines, PLAN) {
+  PLAN[, Face := 1:nrow(PLAN)]
+  # Passage en matrice des coordonnées des vecteurs directeurs des droites
+  matLines <- as.matrix(dtLines[, .SD, .SDcols = colnames(dtLines)[
+    grep("Line_Coo_X", colnames(dtLines))]], nrow = nrow(dtLines))
+  # On récupère les colonnes ptdf
+  col_ptdf <- colnames(PLAN)[grep("ptdf", colnames(PLAN))]
+  ptdf <- matrix(unname(unlist(
+    PLAN[, .SD, .SDcols = col_ptdf])), nrow = length(col_ptdf), byrow = T)
+  ram <- unname(unlist(PLAN[, .SD, .SDcols = "ram"]))
+  
+  # Valeur du produit scalaire dans la formule pour trouver le lambda
+  denom <- matLines %*% ptdf
+  
+  # surcharge de ram si tout d'un coup
+  lambda <- t(ram/t(denom))
+  # surcharge de ram si tout d'un coup
+  lambda[lambda<0] <- 10000000
+  
+  pct1 <- trunc(nrow(matLines)/100)
+  
+  Points <- apply(lambda, 1, function(X)which.min(abs(X)))
+  lambdaout<- apply(lambda, 1, function(X)min(abs(X)))
+  
+  # all(toto == Points$Face)
+  
+  # Points[, Line := rep(1:nrow(Points))]
+  Points <- data.table(Face = Points)
+  Points$lambda <- lambdaout
+  
+  for(i in 1:nrow(ptdf)) {
+    
+    Points[[paste0("Line_Coo_X", i)]] <- matLines[, paste0("Line_Coo_X", i)]
+    
+    Points[[paste0("X", i)]] <- Points$lambda*matLines[, paste0("Line_Coo_X", i)]
+    
+  }
+  
+  Points <- merge(Points, PLAN, by = "Face")
+  Points$distOrig <- abs(Points$lambda)
+  Points
+}
+
+
+evalInter <- function(P1, P2, nbPoints = 50000){
+  
+  col_ptdf <- colnames(P1)[grep("ptdf", colnames(P1))]
+  last_ptdfcol <- col_ptdf[length(col_ptdf)]
+  
+  # Voir peut-être comment rendre ça plus propre
+  PT <- data.table(Line_Coo_X1 = runif(nbPoints) * 30000 - 15000)
+  for (i in 2:(length(col_ptdf)-1)) {
+    PT[, paste0("Line_Coo_X", i) := runif(nbPoints) * 30000 - 15000]
+  }
+
+  col_ptdf <- colnames(P1)[grep("ptdf", colnames(P1))]
+  last_ptdfcol <- col_ptdf[length(col_ptdf)]
+  
+  clcPTin <- function(P, PT, col_ptdf){
+    for (col in col_ptdf[1:(length(col_ptdf)-1)]) {
+      P[[col]] <- P[[col]] - P[[last_ptdfcol]]
+    }
+    
+    re <- as.matrix(P[, .SD, .SDcols = col_ptdf[1:(length(col_ptdf)-1)]])%*%t(PT)
+    which(apply(re, 2, function(X, Y){all(X<Y)}, Y = P$ram))
+  }
+  
+  indomaine1 <- clcPTin(P1, PT, col_ptdf)
+  indomaine2 <- clcPTin(P2, PT, col_ptdf)
+  
+  return(length(intersect(indomaine1, indomaine2))/length(union(indomaine1, indomaine2)))
+  
+}
+evalInter(P1,  P2)
