d1c7bd483d23d0a1990fc97c2d71b992f4103e85
Titouan <titouan.robert@datastorm.fr>
2019-09-17 14:01:34 +0200

    lock code

--- R/graphs.R
+++ R/graphs.R
@@ -929,7 +929,7 @@ plotNetPositionFB <- function(data, dayType,
         
         ipntp <- ipn[which(hour(ipn$time)  == HH  & ipn$mcYear == mcy)]
         
-        dateSel <- unlist(ts[,.SD, .SDcols = (scenario[mcy]$simulation+1)] == DD)
+        dateSel <- unlist(ts[,.SD, .SDcols = as.character(scenario[mcy]$simulation)] == DD)
         dateSel <- ts$Date[dateSel]
         daysel <- substr(dateSel, 6, 10)
         
