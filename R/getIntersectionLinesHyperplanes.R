a4ce7c2282df2b8af1b3178bd6546b856ea2e94c
PPlessiez <paul.plessiez@rte-france.com>
2021-01-14 18:31:59 +0100

    Function evalInter has been improved to allow to specify directions for volume comparison + the overall comparison range.
    Added function evalDomainVolume to calculate a domain volume (the calculation can be done in specific directions).

--- R/getIntersectionLinesHyperplanes.R
+++ R/getIntersectionLinesHyperplanes.R
@@ -119,6 +119,15 @@
 #' @param nbPoints \code{numeric}, number of points generated
 #' @param seed \code{numeric} fixed random seed, used for the weighted draw of the 
 #' typical days. By default, the value is 123456
+#' @param draw_range The range in which the points for the volume assessment should be drawn
+#' @param direction \code{data.table} used to provide specific directions in which
+#' the comparison should be performed. The data table must contain two columns:
+#' \itemize{
+#'  \item a column "country" containing the names of the countries on which a direction is specified
+#'  \item a column "direction" containing a string which can be either "positive" to specify that only
+#'  positive net positions must be considered for this country, or "negative" if only negative net
+#'  positions must be considered
+#' }
 #' 
 #' @examples
 #' \dontrun{
@@ -131,11 +140,13 @@
 #'  evalInter(A = A, B = B, nbPoints = nbPoints)
 #' }
 #' 
-#' @import data.table 
+#' @import data.table
+#' @import stringr
 #' @importFrom stats runif
 #' 
 #' @export
-evalInter <- function(A, B, nbPoints = 50000, seed = 123456){
+
+evalInter <- function(A, B, nbPoints = 50000, seed = 123456, draw_range = c(-15000, 15000), direction = NULL) {
   if (!is.null(seed)) {
     set.seed(seed)
   }
@@ -147,10 +158,17 @@ evalInter <- function(A, B, nbPoints = 50000, seed = 123456){
   last_ptdfcol <- col_ptdf[length(col_ptdf)]
   
   # Voir peut-être comment rendre ça plus propre
-  PT <- data.table(Line_Coo_X1 = runif(nbPoints) * 30000 - 15000)
+  
+  country_1 <- str_remove(col_ptdf[1], "ptdf")
+  
+  country_range <- .findCountryRange(direction, country_1, draw_range)
+  
+  PT <- data.table(Line_Coo_X1 = runif(nbPoints, min = min(country_range), max = max(country_range)))
   if(length(col_ptdf) > 2){
     for (i in 2:(length(col_ptdf)-1)) {
-      PT[, paste0("Line_Coo_X", i) := runif(nbPoints) * 30000 - 15000]
+      current_country <- str_remove(col_ptdf[i], "ptdf")
+      country_range <- .findCountryRange(direction, current_country, draw_range)
+      PT[, paste0("Line_Coo_X", i) := runif(nbPoints, min = min(country_range), max = max(country_range))]
     }
   }
   
@@ -171,5 +189,98 @@ evalInter <- function(A, B, nbPoints = 50000, seed = 123456){
   error2 <- (1-length(intersect(indomaine1, indomaine2))/length(indomaine2))*100
   
   return(data.frame(volIntraInter = volIntraInter, error1 = error1, error2 = error2))
+  
+}
+
+# Find the range within which points should be drawn
+.findCountryRange <- function(direction, country, draw_range) {
+  current_country <- country
+  if(!is.null(direction) & country %in% direction$direction){
+    current_direction <- direction[country == current_country, direction]
+    if(current_direction == "positive"){
+      min_draw <- 0
+      max_draw <- max(draw_range)
+    } else if (current_direction == "negative"){
+      min_draw <- min(draw_range)
+      max_draw <- 0
+    }
+  } else {
+    min_draw <- min(draw_range)
+    max_draw <- max(draw_range)
+  }
+  c(min_draw, max_draw)
+}
+
+#' @title Evaluate the volume of a polyhedron
+#' 
+#' @description This function returns the volume of a polyhedron by calling the function
+#' \code{evalInter} in order to find the shared volume between this polyhedron and a standard
+#' polyhedron which dimension is known.
+#' 
+#' @param A \code{data.table}, polyhedron which volume is to be evaluated, data.table containing at least 
+#' two ptdf columns :
+#' \itemize{
+#'  \item ptdfAT : autrichian vertices
+#'  \item ptdfBE : belgium vertices
+#'  \item ptdfDE : german vertices
+#'  \item ptdfFR : french vertices 
+#' @param nbPoints \code{numeric}, number of points generated
+#' @param seed \code{numeric} fixed random seed, used for the weighted draw of the 
+#' points. By default, the value is 123456
+#' @param assessment_range The range in which the points for the volume assessment should be drawn.
+#' This range should cover the entire domain described by A
+#' @param direction \code{data.table} used to provide specific directions in which
+#' the volume calculation should be performed. The data table must contain two columns:
+#' \itemize{
+#'  \item a column "country" containing the names of the countries on which a direction is specified
+#'  \item a column "direction" containing a string which can be either "positive" to specify that only
+#'  positive net positions must be considered for this country, or "negative" if only negative net
+#'  positions must be considered
+#' }
+#'
+#' @return The volume of polyhedron A
+#' 
+#' @examples
+#' \dontrun{
+#' library(data.table)
+#' polyhedra <- readRDS(system.file("testdata/polyhedra.rds", package = "fbAntares"))
+#' A <- polyhedra[Date == "2019-02-14"]
+#' nbPoints <- 50000
+#' 
+#'  evalDomainVolume(A = A, nbPoints = nbPoints)
+#' }
+#' 
+#' @import data.table
+#' 
+#' @export
+evalDomainVolume <- function(A, nbPoints = 50000, seed = 123456, assessment_range = c(-15000, 15000), direction = NULL){
+  
+  .crtldtFormat(A)
+  .crtlNumeric(nbPoints)
+  col_ptdf <-  .crtlPtdf(A)
+  
+  # B is the reference volume, which volume is known
+  B <- rbindlist(lapply(col_ptdf, function(X){
+    ptdfs <- as.data.table(matrix(0, nrow = 2, ncol = length(col_ptdf) + 1))
+    colnames(ptdfs) <- c(col_ptdf, "ram")
+    ptdfs <- ptdfs[, eval(X) := c(1, -1)]
+    ptdfs <- ptdfs[get(X) == -1, ram := abs(min(assessment_range))]
+    ptdfs <- ptdfs[get(X) == 1, ram := abs(max(assessment_range))]
+    ptdfs
+  }))
+  
+  last_ptdf <- col_ptdf[length(col_ptdf)]
+  B <- B[, eval(last_ptdf) := 0]
+  
+  if(is.null(direction)){
+    volume_B <- (assessment_range[2] - assessment_range[1]) ^ (length(col_ptdf) - 1)
+  } else {
+    volume_B <- (((assessment_range[2] - assessment_range[1]) / 2) ^ nrow(direction)) * ((assessment_range[2] - assessment_range[1]) ^ (length(col_ptdf) - 1 - nrow(direction)))
+  }
+  
+  inter_A_B <- evalInter(A, B, nbPoints, seed, draw_range = assessment_range, direction = direction)
+  shared_volume <- inter_A_B$volIntraInter
+  volume_A <- (shared_volume / 100) * volume_B 
+  volume_A
   
 }
